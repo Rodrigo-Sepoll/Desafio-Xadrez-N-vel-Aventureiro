6d822248db517c5695382ab404a4ee111a18b368
Rodrigo-Sepoll <rodrigo.sepoll@gmail.com>
2025-10-07 00:32:40 +0000

    Criando a definição de constantes para as movimentações das peças

--- Desaafio_Xadrez_Nível_Aventureiro.c
+++ Desaafio_Xadrez_Nível_Aventureiro.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+// Definição de constantes para as movimentações das peças
+int BISPO = 5;   // Bispo: 5 casas na diagonal superior direita
+int TORRE = 5;   // Torre: 5 casas para a direita
+int RAINHA = 8;  // Rainha: 8 casas para a esquerda
+int CAVALO_VERTICAL = 2; // Cavalo: 2 casas em uma direção (vertical)
+int CAVALO_HORIZONTAL = 1; // Cavalo: 1 casa perpendicularmente (horizontal)
 int main(){
 
 
